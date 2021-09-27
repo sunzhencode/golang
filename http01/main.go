85a41ea554ca387a7094b81369b2582d25637a12
sun <jobszhen@163.com>
2021-09-27 16:54:56 +0800

    可以将获取的request 全部返回给response

--- http01/main.go
+++ http01/main.go
@@ -3,19 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func GetMyReuqest(w http.ResponseWriter, r *http.Request) {
 	h := r.Header
 	for k, v := range h {
 		fmt.Println(k, v)
+		value := strings.Join(v, ",")
+		w.Header().Set(k, value)
 	}
 	_, _ = w.Write([]byte("Hello World!"))
 }
 
 func main() {
 	http.HandleFunc("/", GetMyReuqest)
-	err := http.ListenAndServe(":80", nil)
+	err := http.ListenAndServe("0.0.0.0:80", nil)
 	if err != nil {
 		fmt.Println(err)
 	}
