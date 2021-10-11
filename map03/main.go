553561482d7ba7aa0466d75023e3cd614555e51f
sun <jobszhen@163.com>
2021-10-11 10:43:48 +0800

    map练习

--- map03/main.go
+++ map03/main.go
@@ -25,4 +25,10 @@ func Getdiff(arr []int) int {
 func main() {
 	arr := Genslice(100)
 	fmt.Println(Getdiff(arr))
+
+	s3 := []int{1, 2, 3, 4, 5, 6, 7, 8}
+	s4 := s3[3:6]
+	fmt.Printf("The length of s4: %d\n", len(s4))
+	fmt.Printf("The capacity of s4: %d\n", cap(s4))
+	fmt.Printf("The value of s4: %d\n", s4)
 }
