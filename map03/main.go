0e8d7d03444d4653a2441ef0d75725384062994d
sun <jobszhen@163.com>
2021-10-11 10:15:36 +0800

    map练习

--- map03/main.go
+++ map03/main.go
@@ -6,14 +6,23 @@ import (
 )
 
 func Genslice(n int) []int {
-	arr := make([]int, 0, 10)
+	arr := make([]int, 0, n)
 	for i := 0; i < n; i++ {
-		arr = append(arr, rand.Intn(100))
+		arr = append(arr, rand.Intn(128))
 	}
 	return arr
 }
 
+func Getdiff(arr []int) int {
+	m := make(map[int]bool)
+	for _, i := range arr {
+		fmt.Println(len(m))
+		m[i] = true
+	}
+	return len(m)
+}
+
 func main() {
 	arr := Genslice(100)
-	fmt.Println(arr)
+	fmt.Println(Getdiff(arr))
 }
