00f2b30ed88791da6fa76a0bb6cf5eafe17bf308
sun <jobszhen@163.com>
2021-11-03 14:13:20 +0800

    文件操作

--- timeAndFile/main.go
+++ timeAndFile/main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"bufio"
+	"compress/zlib"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
 	"time"
 )
 
@@ -34,11 +40,111 @@ func printSix() {
 
 }
 
+func readAbc(path, outpath string) {
+	//打开写操作的文件
+	fout, err := os.OpenFile(outpath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	//打开读操作的文件
+	f, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	defer fout.Close()
+	writer := bufio.NewWriter(fout)
+	defer f.Close()
+	reader := bufio.NewReader(f)
+
+	for {
+		line, err := reader.ReadString('\n')
+		//line = strings.TrimRight(line, "\n")
+		//fmt.Println(line)
+		//写入读取到的内容
+		writer.WriteString(line)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println("Read file error!", err)
+				return
+			}
+		}
+	}
+	writer.WriteString("\n")
+	writer.Flush()
+	// 按行读取的方法
+	//for {
+	//	if line, _,err := reader.ReadLine(); err != nil {
+	//		if err == io.EOF {
+	//			break
+	//		}
+	//	} else {
+	//		fmt.Println(string(line))
+	//	}
+	//}
+}
+
+func compress(filename, comfile string) {
+	fin, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fout, err := os.OpenFile(comfile, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	bs := make([]byte, 10)
+	writer := zlib.NewWriter(fout) //压缩写入
+	for {
+		n, err := fin.Read(bs)
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err)
+			}
+		} else {
+			writer.Write(bs[:n])
+		}
+	}
+	writer.Close()
+	fout.Close()
+	fin.Close()
+	fin, err = os.Open(comfile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	stat, _ := fin.Stat()
+	fmt.Printf("压缩后文件大小 %dB\n", stat.Size())
+
+	reader, err := zlib.NewReader(fin) //解压
+	io.Copy(os.Stdout, reader)         //把一个流拷贝到另外一个流
+	reader.Close()
+	fin.Close()
+}
+
 //把一个目录下的所有.txt文件合一个大的.txt文件，再对这个大文件进行压缩
 func sumFile() {
-
+	outfile := "abc.txt"
+	os.Remove(outfile)
+	if fileinfos, err := ioutil.ReadDir("."); err == nil {
+		for _, fileinfo := range fileinfos {
+			filename := fileinfo.Name()
+			if strings.HasSuffix(filename, ".txt") {
+				readAbc(filename, outfile)
+			}
+		}
+	}
+	compress(outfile, "abc.zlib")
 }
 
 func main() {
-	timeFmt()
+	sumFile()
 }
