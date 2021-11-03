3f8c012f3a6ad119a43a24a548978b9f7bae14aa
sun <jobszhen@163.com>
2021-11-03 09:31:34 +0800

    map注释

--- map03/main.go
+++ map03/main.go
@@ -5,15 +5,17 @@ import (
 	"math/rand"
 )
 
+//slice扩容规则实验
 func Genslice(n int) []int {
 	arr := make([]int, 0, 10)
 	for i := 0; i < n; i++ {
 		arr = append(arr, rand.Intn(128))
-		fmt.Println(cap(arr))
+		//fmt.Println(cap(arr))
 	}
 	return arr
 }
 
+//1.创建一个初始长度为0、容量为10的int型切片，调用rand.Intn(128)100次，往切片里面添加100个元素，利用map统计该切片里有多少个互不相同的元素。
 func Getdiff(arr []int) int {
 	m := make(map[int]bool)
 	for _, i := range arr {
